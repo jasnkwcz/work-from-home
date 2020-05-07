ab3c183752dd61909e8bd195d8637f89b88425ff
Daniel Flynn <flynndcs@gmail.com>
2020-05-06 21:03:14 -0500

    changed api/client structure to allow AWS deployment

---
+++ client/build/precache-manifest.64d2dba05cfd335571f1015251be13d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b067ae9628238d3a22faff26ab4801e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3477f75fc364ef1163b9",
+    "url": "/static/css/main.80c5dc8c.chunk.css"
+  },
+  {
+    "revision": "c49d2416271f9308e6f3",
+    "url": "/static/js/2.f877b6ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f877b6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3477f75fc364ef1163b9",
+    "url": "/static/js/main.f8181a8f.chunk.js"
+  },
+  {
+    "revision": "0fa492b7c6be91572883",
+    "url": "/static/js/runtime-main.775d47d6.js"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,7 @@ import WorkFromHome from './WorkFromHome';
 class App extends Component {
 	constructor(props){
 		super(props);
-		this.state = { apiResponse: "" };
-	}	
-	callAPI(){
-		fetch("http://localhost:9000/testAPI")
-			.then(res => res.text())
-			.then(res => this.setState({ apiResponse: res }))
-			.catch(err => err);
-	}
-
-	componentDidMount() {
-		this.callAPI();
+		this.state = { apiResponse: "placeholder API message" };
 	}
 
 	render() {
